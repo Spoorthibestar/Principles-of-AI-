22381b31b012e13afea247c821e64c442f297a50
SPOORTHI . K <151846077+Spoorthibestar@users.noreply.github.com>
2024-05-14 21:26:18 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,29 @@
+print("Hello, World!")
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+print("4")
+
+x = "Python is awesome"
+print(x)
+
+print(10 + 5)
+
+x = 5
+print(type(x))
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+print(tuple1,tuple2,tuple3)
