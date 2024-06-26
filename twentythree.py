101c8f8265ebbd7d2b47c593ce1c586af9ff7bc6
SPOORTHI . K <151846077+Spoorthibestar@users.noreply.github.com>
2024-06-26 20:44:19 +0530

    Add files via upload

---
+++ twentythree.py
@@ -0,0 +1,9 @@
+# Function to reverse a string
+def reverse_string(s):
+    return s[::-1]
+
+# Input a string
+string = input("Enter a string: ")
+
+# Display the reversed string
+print("The reversed string is:", reverse_string(string))
