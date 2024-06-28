5fb00a2cce0d813f8b48dd73f0ecb4a653d4710a
SPOORTHI . K <151846077+Spoorthibestar@users.noreply.github.com>
2024-06-28 18:38:06 +0530

    Add files via upload

---
+++ twentynine.py
@@ -0,0 +1,9 @@
+# Function to convert a string to uppercase
+def to_uppercase(s):
+    return s.upper()
+
+# Input a string
+string = input("Enter a string: ")
+
+# Display the uppercase string
+print("Uppercase string:", to_uppercase(string))
