5940c2e3ece5a4f3a02f2ebf297dba84236ab2aa
SPOORTHI . K <151846077+Spoorthibestar@users.noreply.github.com>
2024-06-30 16:24:49 +0530

    Add files via upload

---
+++ thirtyfour.py
@@ -0,0 +1,11 @@
+import math
+
+# Function to find the square root of a number
+def square_root(num):
+    return math.sqrt(num)
+
+# Input a number
+number = float(input("Enter a number: "))
+
+# Display the square root
+print("The square root of", number, "is", square_root(number))
